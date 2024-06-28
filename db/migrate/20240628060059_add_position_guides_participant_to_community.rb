d2da03586d27bfce6626e86df25411d7f2e5d23b
minmaung132h <minmaung132h@prestigein.com>
2024-06-28 15:21:19 +0700

    fixed db/migrate

--- db/migrate/20240628060059_add_position_guides_participant_to_community.rb
+++ db/migrate/20240628060059_add_position_guides_participant_to_community.rb
@@ -1,4 +1,4 @@
-class AddPositionGuidesParticipantToCommunity < ActiveRecord::Migration[7.1]
+class AddPositionGuidesParticipantToCommunity < ActiveRecord::Migration[7.0]
   def change
     add_column :patchwork_communities, :position, :integer, default: 0
     add_column :patchwork_communities, :guides, :jsonb, default: {}
